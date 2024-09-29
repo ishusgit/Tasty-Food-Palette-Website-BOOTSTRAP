05a229717c463849b84990ff14c9d361f2e8e3cd
ishusgit <iswarya.harish25@gmail.com>
2024-09-29 17:45:33 +0700

    Features:responsive and scroll section and animations

--- js/script.js
+++ js/script.js
@@ -1,31 +1,50 @@
 //back to top btn
-function userScroll(){
-const navbar = document.querySelector('.navbar');
-const backtoTop = document.querySelector('#back-to-top');
-window.addEventListener('scroll', () =>{
-  if(window.scrollY > 50){
-    navbar.classList.add('navbar-sticky');
-    //navbar.classList.add('about-bg');
-    backtoTop.classList.add('show');
-  }else{
-    navbar.classList.remove('navbar-sticky');
-    //navbar.classList.add('about-bg');
-    backtoTop.classList.remove('show');
-  }
-});
-}
-function scrollToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+// function userScroll(){
+// const navbar = document.querySelector('.navbar');
+// const backtoTop = document.querySelector('#back-to-top');
+// window.addEventListener('scroll', () =>{
+//   if(window.scrollY > 50){
+//     navbar.classList.add('navbar-sticky');
+//     //navbar.classList.add('about-bg');
+//     backtoTop.classList.add('show');
+//   }else{
+//     navbar.classList.remove('navbar-sticky');
+//     //navbar.classList.add('about-bg');
+//     backtoTop.classList.remove('show');
+//   }
+// });
+// }
+// function scrollToTop() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
 
 //eventlistener
-document.addEventListener('DOMContentLoaded', userScroll);
-document.querySelector('#back-to-top'), addEventListener('click',scrollToTop);
-//prvent the back to top function from interfering with carousel buttons
-document.querySelectorAll('.carousel-control-prev, .navbar-toggler, .carousel-control-next, .nav-tabs, .accordion, [data-bs-toggle=popover], [data-bs-toggle=tooltip]').forEach(button =>{
-  button.addEventListener('click', function(event){
-      event.preventDefault();
-      event.stopPropagation();
+// document.addEventListener('DOMContentLoaded', userScroll);
+// document.querySelector('#back-to-top'), addEventListener('click',scrollToTop);
+// //prvent the back to top function from interfering with carousel buttons
+// document.querySelectorAll('.carousel-control-prev, .navbar-toggler, .carousel-control-next, .nav-tabs, .accordion, [data-bs-toggle=popover], [data-bs-toggle=tooltip]').forEach(button =>{
+//   button.addEventListener('click', function(event){
+//       event.preventDefault();
+//       event.stopPropagation();
+//   });
+//  });
+
+
+//scroll triggered animation- scroll to corresponding section content should come from left and right
+ document.addEventListener('DOMContentLoaded', function(){
+  const sections = document.querySelectorAll('.scroll-section');
+  const observer = new IntersectionObserver(entries =>{
+    entries.forEach(entry => {
+      if(entry.isIntersecting){
+        entry.target.classList.add('visible');
+      } else {
+        entry.target.classList.remove('visible');
+       }
+    });
+  }, { threshold:0.1 });
+  
+    sections.forEach(section =>{
+      observer.observe(section);
   });
  });
